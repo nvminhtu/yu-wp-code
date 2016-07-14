9fa1ece54151436f65cfdd948808765bf20940d4
nvminhtu <nvminhtu@gmail.com>
2016-07-14 09:12:32 +0700

    custom post type with custom post type permalink

--- yumerasen-test/inc/custom-post-types.php
+++ yumerasen-test/inc/custom-post-types.php
@@ -2,6 +2,9 @@
   /***
   *** CPT: Create custom post types
   ***/
+
+/* Step 1 - CPT: Register Post type
+----------------------------------------------*/
 function prefix_register_post_type(){
 	register_post_type(
 		'shop',
@@ -38,12 +41,85 @@ function prefix_register_post_type(){
         'page-attributes'
 			),
 			'has_archive'   => true,
-			'rewrite'       => array(
-				'slug' => 'shop',
-			),
+			'rewrite' => false
 		)
 	);
 }
 add_action('init', 'prefix_register_post_type');
 
+/* Step 2 - CPT: Setup %Tag% for permalink
+----------------------------------------------*/
+global $wp_rewrite;
+$shop_structure = '/shop/%shop%';
+$wp_rewrite->add_rewrite_tag("%shop%", '([^/]+)', "shop=");
+$wp_rewrite->add_permastruct('shop', $shop_structure, false);
+
+/* Step 3 - CPT: Translate Post type link for permalink
+----------------------------------------------*/
+add_filter('post_type_link', 'shop_permalink', 10, 3);
+//@Adapted from get_permalink function in wp-includes/link-template.php
+function shop_permalink($permalink, $post_id, $leavename) {
+  $post = get_post($post_id);
+  $rewritecode = array(
+      '%year%',
+      '%monthnum%',
+      '%day%',
+      '%hour%',
+      '%minute%',
+      '%second%',
+      $leavename? '' : '%postname%',
+      '%post_id%',
+      '%category%',
+      '%author%',
+      $leavename? '' : '%pagename%',
+  );
+
+  if ( '' != $permalink && !in_array($post->post_status, array('draft', 'pending', 'auto-draft')) ) {
+      $unixtime = strtotime($post->post_date);
+
+      $category = '';
+      if ( strpos($permalink, '%category%') !== false ) {
+          $cats = get_the_category($post->ID);
+          if ( $cats ) {
+              usort($cats, '_usort_terms_by_ID'); // order by ID
+              $category = $cats[0]->slug;
+              if ( $parent = $cats[0]->parent )
+                  $category = get_category_parents($parent, false, '/', true) . $category;
+          }
+          // show default category in permalinks, without
+          // having to assign it explicitly
+          if ( empty($category) ) {
+              $default_category = get_category( get_option( 'default_category' ) );
+              $category = is_wp_error( $default_category ) ? '' : $default_category->slug;
+          }
+      }
+
+      $author = '';
+      if ( strpos($permalink, '%author%') !== false ) {
+          $authordata = get_userdata($post->post_author);
+          $author = $authordata->user_nicename;
+      }
+
+      $date = explode(" ",date('Y m d H i s', $unixtime));
+      $rewritereplace =
+      array(
+          $date[0],
+          $date[1],
+          $date[2],
+          $date[3],
+          $date[4],
+          $date[5],
+          $post->post_name,
+          $post->ID,
+          $category,
+          $author,
+          $post->post_name,
+      );
+      $permalink = str_replace($rewritecode, $rewritereplace, $permalink);
+  }
+  else { // if they're not using the fancy permalink option
+
+  }
+  return $permalink;
+}
 ?>
