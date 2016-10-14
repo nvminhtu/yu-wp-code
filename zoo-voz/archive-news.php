746c6dc4c424f36710a3dd84496cbd1b0ea2c5a5
nvminhtu <nvminhtu@gmail.com>
2016-10-14 21:40:36 +0700

    Dat add archive news layout

---
+++ zoo-voz/archive-news.php
@@ -0,0 +1,73 @@
+<?php get_header(); ?>
+<!-- main start -->
+<div id="main" class="clearfix">
+  <div class="inner ftb">
+    <div id="topic_path" class="clearfix">
+      <ul>
+        <li><a href="<?php bloginfo('siteurl'); ?>">HOME</a> &gt; </li>
+        <li>お知らせ</li>
+      </ul>
+    </div>
+    <div id="container" class="clearfix">
+      <div id="content" class="clearfix">
+        <h3 class="title_lblog">すべての記事一覧</h3>
+        <div class="box_archive_news clearfix">
+        
+        <article class="l-article-list link-item">
+    <!--画像とNEWタグ-->
+    <div class="l-article-list-left">
+        <a href="https://liginc.co.jp/316693">
+            <div class="media media-article-list-eyecatch">		<img class="js_lazy" src="http://www.groovoost.com/wp-content/uploads/2016/10/google-analytic.png" data-original="https://cdn.liginc.co.jp/wp-content/uploads/2016/10/147617340424255300_84-400x267.jpg" alt="316693沖縄に住みたいけど仕事ってあるの？実際の企業に就職事情を聞いてきた。のアイキャッチ" style="display: inline;">
+		</div>
+        </a>
+        <!--殿堂入り-->
+                <!--NEWタグ-->
+                    <div class="l-article-list-tag">
+                <img src="/wp-content/themes/ligtheme/images/svg/icon-new.svg" alt="new">
+            </div>
+            </div><!-- /.top-article-list-left -->
+    <!--右側-->
+    <div class="l-article-list-right">
+        <a href="https://liginc.co.jp/316693">
+            <h1 class="heading heading-list-general">沖縄に住みたいけど仕事ってあるの？実際の企業に就職事情を聞いてきた。</h1>
+        </a>
+        <div class="l-article-list-right-middle">
+						<a href="https://liginc.co.jp/series/moreiioffice" class="link series-link">
+                <span class="heading heading-list-series">
+                    もっと、いいオフィス。                </span>
+			</a>
+			                            <span class="tag tag-pr tag-series">LIGPR</span>
+                    </div><!-- /.top-article-list-right-middle -->
+        <div class="l-article-list-right-bottom">
+            <!--この記事を書いた人-->
+            <a href="https://liginc.co.jp/member/member_detail?user=seko">
+                <div class="author author-top">
+                    <div class="author-top-avatar">
+                        <img src="https://cdn.liginc.co.jp/wp-content/uploads/2015/10/18ffe47ae091488567f5b7bacfbe9db751.jpg" width="40" height="40" alt="勢古口">
+                    </div>
+                    <span class="author-top-name">勢古口</span>
+                </div><!-- /.media_meta_box_auther -->
+            </a>
+            <!--日付-->
+            <div class="l-article-list-date-count">
+                <time datetime="2016-10-14" class="date date-list-general">
+                    2016.10.14                </time>
+            <!--いいね数-->
+            <span class="sns sns-count sns-count-top"><i class="icon-heart"></i>89</span>
+            </div><!-- /.top-article-list-right-bottom -->
+        </div><!-- /.top-article-list-right-bottom -->
+    </div><!-- /.top-article-list-right -->
+</article>
+
+			
+          <!--end news list--></div>
+
+        <p class="btn btn01 btn_hblue btn_mw500"><a id="more_blog" href="javascript:void(0)"><span>もっと見る</span></a></p>
+      </div>
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
+</div>
+
+<!-- main end -->
+<?php get_footer(); ?>
